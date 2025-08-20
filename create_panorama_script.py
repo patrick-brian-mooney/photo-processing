c0cf69788c25ce7299ded763b8d85fb5e1ebed5a
Patrick Mooney <github.pmooney78@neverbox.com>
2025-08-20 14:19:41 -0500

    Update create_panorama_script.py to use more modern Python coding style
    Updates some documentation; uses better PEP8 compliance; uses Path objects
    instead of strings to represent file paths; factors some logic out of main
    function and into code that calls it; uses f-strings instead of % interpolation.

--- create_panorama_script.py
+++ create_panorama_script.py
@@ -12,7 +12,7 @@ off automatically with this script is faster overall for large projects than
 doing everything manually from the beginning would be.
 
 A short (i.e., non-comprehensive) list of choices the output script makes for
-you would include:
+you might include:
     * using CPFind as the control point detector;
     * continuously overwriting the same project file instead of leaving
       multiple project files behind to allow for problem tracing;
@@ -20,7 +20,7 @@ you would include:
       filename) as the reference (or "anchor") image for the purposes of both
       position and exposure, which often winds up not being the best choice;
     * assuming that the input images are taken with a rectilinear lens;
-    * running Celeste;
+    * running Celeste to remove control points on clouds;
     * running CPFind's version of Celeste instead of Celeste standalone;
     * using the --multirow match detection algorithm, which is generally
       pretty good, but which is not perfect for all possible scenarios, and
@@ -39,12 +39,15 @@ you would include:
     * using hugin_executor as the stitching program (PTBatchGUI might also be
       used for this purpose).
 
+It also functions as a Python module in which some of these decisions,
+including but not limited to which files are used, can be controlled by
+changing the parameters passed to the produce_script() function.
 
 This program comes with ABSOLUTELY NO WARRANTY. Use at your own risk.
 
 This program is part of Patrick Mooney's photo postprocessing scripts; the
 complete set can be found at https://github.com/patrick-brian-mooney/photo-processing.
-All programs in that collection are copyright 2015-2019 by Patrick Mooney; they
+All programs in that collection are copyright 2015-2025 by Patrick Mooney; they
 are free software released under the GNU GPL, either version 3 or (at your
 option) any later version. See the file LICENSE.md for details.
 
@@ -53,38 +56,50 @@ The latest version of these scripts can always be found at
 """
 
 
-import os, glob, shlex
+import glob
+import os
+import shlex
+
+from typing import Sequence
+from pathlib import Path
 
 
-def produce_script(the_files=None):
-    if not the_files:
-        the_files = sorted(list(set(glob.glob('*JPG') + glob.glob('*jpg'))))
-    the_files_list = ' '.join([shlex.quote(f) for f in the_files])
+def produce_script(the_files: Sequence[Path]):
+    """Produce the actual script.
+    """
+    assert isinstance(the_files, Sequence)
+    assert len(the_files) > 2, "ERROR! You must specify at least two files to make into a panorama!"
+    assert all([isinstance(f, Path) for f in the_files]), "ERROR! FIles passed to produce_script must be Paths!"
+
+    the_files_list = ' '.join([shlex.quote(str(f)) for f in the_files])
     project_file = shlex.quote(the_files[0] + ".pto")
     if the_files:
-        the_script = """#!/usr/bin/env bash
+        the_script = f"""#!/usr/bin/env bash
 # This script written by Patrick Mooney's create_panorama_script.py script, see
 #     https://github.com/patrick-brian-mooney/photo-processing/blob/master/create_panorama_script.py
-pto_gen -o %s %s
-""" % (project_file, the_files_list)
+pto_gen -o {project_file} {the_files_list}
+"""
 
-        the_script = the_script + """
-cpfind --multirow --celeste -o %s %s
-cpclean -o %s %s
-linefind -o %s %s
-autooptimiser -a -l -s -m -o %s %s
-pano_modify --canvas=AUTO --crop=AUTO -o %s %s
-# hugin_executor -s %s                              # Uncomment to stitch the panorama immediately
-""" % tuple([project_file] * 11)
+        the_script += f"""
+cpfind --multirow --celeste -o {project_file} {project_file}
+cpclean -o {project_file} {project_file}
+linefind -o {project_file} {project_file}
+autooptimiser -a -l -s -m -o {project_file} {project_file}
+pano_modify --canvas=AUTO --crop=AUTO -o {project_file} {project_file}
+# hugin_executor -s {project_file}                              # Uncomment to stitch the panorama immediately
+"""
 
-        script_file_name = os.path.splitext(the_files[0])[0] + '-pano.SH'
-        with open(script_file_name, mode='w') as script_file:
+        script_file_name = the_files[0].stem + '-pano.SH'
+        with open(script_file_name, mode='w', encoding='utf-8') as script_file:
             script_file.write(''.join(the_script))
 
-        os.chmod(script_file_name, os.stat(script_file_name).st_mode | 0o111)    # or, in Bash, "chmod a+x SCRIPT_FILE_NAME"
-    else:
-        raise IndexError('You must call create_panorama_script.py in a folder with at least one .jpg or .JPG file;\n   current working directory is %s' % os.getcwd())
+        os.chmod(script_file_name, os.stat(script_file_name).st_mode | 0o111)    # `chmod a+x SCRIPT_FILE_NAME`
 
 
 if __name__ == "__main__":
-    produce_script()
+    files = sorted(list(set(glob.glob('*JPG') + glob.glob('*jpg'))))
+    if not files:
+        print('You must call create_panorama_script.py in a folder with at least one .jpg or .JPG file!')
+        print(f'   current working directory is {os.getcwd()}')
+
+    produce_script([Path(f) for f in files])
